06a8d35ccfc4acc463b91a22d8cc7b73ff7ba224
André Duarte <140335@corp.hrln.br>
2024-11-06 18:01:37 -0300

    terminando fase 3

--- src/models/Autor.js
+++ src/models/Autor.js
@@ -4,14 +4,15 @@ const autorSchema = new mongoose.Schema(
   {
     id: {type: String},
     nome: {
-      type: String, 
-      required: [true, "O nome do(a) autor(a) é obrigatório"]},
+      type: String,
+      required: [true, "O nome do(a) autor(a) é obrigatório"]
+    },
     nacionalidade: {type: String}
   },
   {
     versionKey: false
   }
-)
+);
 
 const autores = mongoose.model("autores", autorSchema)
 
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,16 +4,35 @@ const livroSchema = new mongoose.Schema(
   {
     id: {type: String},
     titulo: {
-      type: String, 
-      required: [true, "O título do livro é obrigatório"]},
+      type: String,
+      required: [true, "O título do livro é obrigatório"]
+    },
     autor: {
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: "autores", 
-      required: [true, "O(a) autor(a) é obrigatório"]},
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "autores",
+      required: [true, "O(a) autor(a) é obrigatório"]
+    },
     editora: {
-      type: String, 
-      required: [true, "A editora é obrigatório"]},
-    numeroPaginas: {type: Number}
+      type: String,
+      required: [true, "A editora é obrigatória"],
+      enum: {
+        values: ["Casa do código", "Alura"],
+        message: "A editora {VALUE} não é um valor permitido."
+      }
+    },
+    numeroPaginas: {
+      type: Number,
+        validate: {
+          validator: (valor) => {
+          return valor >= 10 && valor <= 5000;
+        },
+        message: "O número de páginas deve estar entre 10 e 5000.Valor fornecido: {VALUE}."
+      }
+      /*
+      min: [10, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"],
+      max: [5000, "O número de páginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}"]
+      */
+    }
   }
 );
 
