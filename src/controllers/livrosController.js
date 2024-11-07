067fe08610d09169b8f66651c235cd2c39a51370
André Duarte <140335@corp.hrln.br>
2024-11-07 18:00:11 -0300

    finalizando o curso

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,34 +1,16 @@
 import NaoEncontrado from "../erros/NaoEncontrado.js";
-import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js";
 import { autores, livros } from "../models/index.js";
 
 class LivroController {
 
   static listarLivros = async (req, res, next) => {
-    try {
-
-      let {limite = 5, pagina = 1, ordenacao = "_id:-1" } = req.query;
-
-      let [campoOrdenacao, ordem] = ordenacao.split(":");
-
-      limite = parseInt(limite);
-      pagina = parseInt(pagina);
-      ordem = parseInt(ordem);
-
-      if (limite > 0  && pagina > 0){
-        const livrosResultado = await livros.find()
-          .sort({ [campoOrdenacao]: ordem })
-          .skip((pagina - 1) * 5)
-          .limit(limite)
-          .populate("autor")
-          .exec();
-
-        res.status(200).json(livrosResultado);
-      } else {
-        next(new RequisicaoIncorreta());
-      }
+    try{
+      const buscaLivros = livros.find()
       
-     
+      req.resultado = buscaLivros;
+
+      next();
+
     } catch (erro) {
       next(erro);
     }
@@ -102,11 +84,13 @@ class LivroController {
       const busca = await processaBusca(req.query);
 
       if(busca !== null){
-        const livrosResultado = await livros
+        const livrosResultado = livros
         .find(busca)
         .populate("autor");
-  
-        res.status(200).send(livrosResultado);
+
+        req.resultado = livrosResultado;
+
+        next();
       }else{
         res.status(200).send([]);
       }
